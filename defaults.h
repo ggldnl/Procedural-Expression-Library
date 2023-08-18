3d2d461b95afe13ea97ebf797ac51814b7f37e8f
Re-l <danielgigliotti99.dg@gmail.com>
2023-08-18 10:25:06 +0200

    added defaults

---
+++ defaults.h
@@ -0,0 +1,82 @@
+#ifndef DEFAULTS_H
+#define DEFAULTS_H
+
+namespace gui_defaults {
+  
+  /**
+   * We draw the left and right eye starting from the current 
+   * positoon (current_x, current_y). We subtract half the 
+   * width of the bounding box to obtain the center of the left eye
+   * and add half the width to obtain the center of the right eye. 
+   * We subtract LEFT_EYE_DISTANCE_FROM_CENTER to further shift 
+   * the left eye by the specified number of pixels on the left.
+   * Same thing for the right eye (RIGHT_EYE_DISTANCE_FROM_CENTER).
+   */
+  constexpr uint16_t LEFT_EYE_DISTANCE_FROM_CENTER = 4;
+  
+  /**
+   * We draw the left and right eye starting from the current 
+   * positoon (current_x, current_y). We subtract half the 
+   * width of the bounding box to obtain the center of the left eye
+   * and add half the width to obtain the center of the right eye. 
+   * We subtract LEFT_EYE_DISTANCE_FROM_CENTER to further shift 
+   * the left eye by the specified number of pixels on the left.
+   * Same thing for the right eye (RIGHT_EYE_DISTANCE_FROM_CENTER).
+   */
+  constexpr uint16_t RIGHT_EYE_DISTANCE_FROM_CENTER = 4;
+
+  /**
+   * Minimum bounding box height.
+   */
+  constexpr uint16_t MIN_BBOX_HEIGHT = 20;
+
+  /**
+   * Maximum bounding box height.
+   */
+  constexpr uint16_t MAX_BBOX_HEIGHT = 30;
+
+  /**
+   * Minimum bounding box width.
+   */
+  constexpr uint16_t MIN_BBOX_WIDTH = 10;
+
+  /**
+   * Maximum bounding box width.
+   */
+  constexpr uint16_t MAX_BBOX_WIDTH = 20;
+
+  /**
+   * When looking up and down, the eye bounding box height get
+   * squished.
+   */
+  constexpr bool VERTICAL_PERSPECTIVE_ENABLED = true;
+  
+  /**
+   * When looking left and right, the eye bouding box width get
+   * squished.
+   */
+  constexpr bool HORIZONTAL_PERSPECTIVE_ENABLED = true;
+
+  /**
+   * When moving from the current position to the target one, 
+   * in order to mimic an organic movement the distance is first
+   * computed and then a fraction of it is used as increment in
+   * the direction of the movement. When the eye almost reaches
+   * the target position and its distance is less than this 
+   * threshold, the current position is set to the target one
+   * without further computations. 
+   */
+  constexpr uint16_t MIN_PIXEL_DISTANCE_THRESHOLD = 2;
+
+  /**
+   * Number of frames for the interpolation animation.
+   */
+  constexpr uint16_t INTERPOLATION_ANIMATION_DEFAULT_STEPS = 4;
+
+  /**
+   * Number of frames for the blinking animation.
+   */
+  constexpr uint16_t BLINKING_ANIMATION_DEFAULT_STEPS = 4;
+}
+
+#endif
